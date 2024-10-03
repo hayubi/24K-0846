e7b8914ff503afcf48b17cdfb2a2b483af0ddbc8
hayubi <hayubi53@gmail.com>
2024-10-03 13:20:29 +0500

    Update task1.c

--- lab6/task1.c
+++ lab6/task1.c
@@ -1,5 +1,7 @@
 /*1. Which loop system would be better for user input. Justify your answer by creating a program that
-takes a value and adds it to a variable and prints it repeatedly until the user enters a zero value.*/
+takes a value and adds it to a variable and prints it repeatedly until the user enters a zero value.
+
+while loop system as we want sum all the numbers until the user inputs (indefinite inputs)*/
 
 #include <stdio.h>
 int main()
