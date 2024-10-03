fecd7d937436dc484b2c2fde33d0e65fa0789914
hayubi <hayubi53@gmail.com>
2024-10-03 13:18:16 +0500

    Add files via upload

---
+++ lab6/task1.c
@@ -0,0 +1,15 @@
+/*1. Which loop system would be better for user input. Justify your answer by creating a program that
+takes a value and adds it to a variable and prints it repeatedly until the user enters a zero value.*/
+
+#include <stdio.h>
+int main()
+{
+int num, sum=0;
+while (num != 0)
+{
+	printf("enter a number\n");
+	scanf("%d", &num);
+	sum = sum + num;
+	printf("sum = %d\n", sum);
+}
+}
