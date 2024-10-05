45253595dd5b531552af5a2714f89d42f2ec37aa
hayubi <hayubi53@gmail.com>
2024-10-05 14:38:36 +0500

    Add files via upload

---
+++ lab6/task7.c
@@ -0,0 +1,22 @@
+/*
+7. Make another pattern like this using characters
+A B C D E
+B       F
+C       G
+D       H
+E       I
+F G H I J
+*/
+
+#include <stdio.h>
+int main()
+{
+int i;
+for (i=0; i<6; i++)
+{	
+	if ((i==0) || (i==5))
+		{printf("%c %c %c %c %c\n", i+65, i+66, i+67, i+68, i+69);}
+	else
+		{printf("%c       %c\n", i+65, i+69);}
+}
+}
