0ef92ced458c06c8a3d1717f807e73ce57758c70
hayubi <hayubi53@gmail.com>
2024-10-05 14:28:27 +0500

    Add files via upload

---
+++ lab6/task6.c
@@ -0,0 +1,22 @@
+/*
+6. Make an opposite of the above-mentioned pattern using do-while loop
+*       *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*       *
+*/
+
+#include <stdio.h>
+int main()
+{
+int i;
+for (i=0; i<6; i++)
+{
+	if ((i==0) || (i==5))
+		{printf("*       *\n");}
+	else
+		{printf("* * * * *\n");}
+}
+}
