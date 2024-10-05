4ced7a178e36cac6111f3d1dc778f6ca371fd522
hayubi <hayubi53@gmail.com>
2024-10-05 15:02:25 +0500

    Add files via upload

---
+++ lab6/task8 alternative.c
@@ -0,0 +1,23 @@
+/*
+8 alternative.
+* * * * *
+  *   *  
+    *    
+  *   *  
+* * * * *
+*/
+
+#include <stdio.h>
+int main()
+{
+int i;
+for (i=0; i<5; i++)
+{
+	if ((i==0) || (i==4))
+		{printf("* * * * *\n");}
+	else if ((i==1) || (i==3))
+		{printf("  *   *  \n");}
+	else
+		{printf("    *    \n");}
+}
+}
