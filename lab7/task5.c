284e8f4bc0b5b5884be347ca03b29666773f2378
hayubi <hayubi53@gmail.com>
2024-10-24 23:29:54 +0500

    Update task5.c

--- lab7/task5.c
+++ lab7/task5.c
@@ -47,3 +47,32 @@ int main ()
     else 
         printf ("it is a palindrome");
 }
+
+
+
+//q5
+#include<stdio.h>
+int main()
+{
+  char str[100];
+  int palindrome = 1, length=0;
+  printf("Enter a string: ");
+  scanf("%s", str);
+
+  // To find out the length of the string, we need to see when the '\0' terminator appears in the string
+  for(int i=0 ; str[i] != '\0' ; i++)
+     {length++;}
+
+  // Checking for palindrome
+  for(int i=0 ; i<length ; i++)
+     {if(str[i] != str[length-1-i])
+        {palindrome = 0;} 
+         break;}
+
+  // Printing if string is palindrome or not
+  if(palindrome == 1)
+    {printf("The string entered is a palindrome.");}
+  else if(palindrome == 0)
+    {printf("The string entered is not a palindrome.");}
+  return 0;
+}
