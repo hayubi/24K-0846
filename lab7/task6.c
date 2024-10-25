6791ce8ea78ee9e0588247b2f7190f725b2ce64d
hayubi <hayubi53@gmail.com>
2024-10-25 09:46:55 +0500

    Add files via upload

--- lab7/task6.c
+++ lab7/task6.c
@@ -1,28 +1,34 @@
 /*Write a program to read 10 integers into an array. Then, use loops to find the
 frequency of each element in the array (how many times each number occurs).*/
 
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int array[10];
-    int counter[1000]={0};
-    int i,size=10;
-    printf("Enter 10 numbers:\n");
-    for(i=0;i<size;i++)
+    int frequency[100];
+    int i;
+
+    //reading integres in the array
+    for(i=0; i<10; i++)
     {
-        printf("Number %d: ",i+1);
-        scanf("%d",&array[i]);
+        printf("enter number for index %d: ", i);
+        scanf("%d", &array[i]);
     }
+
+    //initialising frequency array to 0
+    for(i=0; i<10; i++)
+        {frequency[i]=0;}
     
-    for(int i=0;i<size;i++)
-        {counter[array[i]]++;}
+    //counting frequency of each elemnt in the array
+    for(i=0; i<10; i++)
+        {frequency[array[i]] = frequency[array[i]] + 1;}
     
-    printf("\nElements Summary\n");
-    for(i=0;i<size;i++)
+    for(i=0; i<10; i++)
     {
-        if(counter[array[i]]>=1)
+        if (frequency[array[i]] >= 1)
         {
-            printf("Element %d comes %d times\n",array[i],counter[array[i]]);
-            counter[array[i]]=0;
-        }   
+            printf("\nNumber %d comes %d times.", array[i], frequency[array[i]]);
+            frequency[array[i]] = 0;
+        }
     }
 }
